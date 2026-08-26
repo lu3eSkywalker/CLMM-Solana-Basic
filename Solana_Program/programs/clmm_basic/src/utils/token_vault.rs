31d48229a6e3c01a3e9e2de5e3ae5bad3ffc3fcb
lu3eskywalker <surajjhajhria1@gmail.com>
2026-08-26 23:57:01 +0530

    feat: added token transfer logic

--- Solana_Program/programs/clmm_basic/src/utils/token_vault.rs
+++ Solana_Program/programs/clmm_basic/src/utils/token_vault.rs
@@ -1,7 +1,9 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{InitializeAccount, initialize_account};
+use anchor_spl::token::{InitializeAccount, initialize_account, Transfer, transfer};
+use crate::state::Pool;
 
 pub const TOKEN_ACCOUNT_SIZE: usize = 165;
+pub const POOL_SEED: &str = "pool";
 
 pub fn create_token_vault_account<'info>(
     vault_account: &mut AccountInfo<'info>,
@@ -36,4 +38,62 @@ pub fn create_token_vault_account<'info>(
     initialize_account(init_account_ctx)?;
 
     Ok(())
+}
+
+pub fn transfer_from_user_to_pool_vault<'info>(
+    signer: &AccountInfo<'info>,
+    from: &AccountInfo<'info>,
+    to_vault: &AccountInfo<'info>,
+    token_program: &AccountInfo<'info>,
+    amount: u64,
+) -> Result<()> {
+    if amount == 0 {
+        return Ok(());
+    }
+
+    transfer(
+        CpiContext::new(
+            token_program.to_account_info(),
+            Transfer {
+                from: from.to_account_info(),
+                to: to_vault.to_account_info(),
+                authority: signer.to_account_info(),
+            },
+        ),
+        amount,
+    )
+}
+
+pub fn transfer_from_pool_vault_to_user<'info>(
+    pool: &Account<'info, Pool>,
+    from_vault: &AccountInfo<'info>,
+    to: &AccountInfo<'info>,
+    token_program: &AccountInfo<'info>,
+    amount: u64,
+) -> Result<()> {
+    let bump = [pool.bump];
+
+    let signer_seeds: &[&[u8]] = &[
+        POOL_SEED.as_bytes(),
+        pool.token0_mint.as_ref(),
+        pool.token1_mint.as_ref(),
+        &bump,
+    ];
+
+    if amount == 0 {
+        return Ok(());
+    }
+
+    transfer(
+        CpiContext::new_with_signer(
+            token_program.to_account_info(),
+            Transfer {
+                from: from_vault.to_account_info(),
+                to: to.to_account_info(),
+                authority: pool.to_account_info(),
+            },
+            &[signer_seeds],
+        ),
+        amount,
+    )
 }
